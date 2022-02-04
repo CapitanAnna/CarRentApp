1694da8a6c2dc1d8733c36d9e08583d5fec6a628
CapitanAnna <capitanstil@gmail.com>
2022-02-04 10:03:33 +0200

    Add main window, check if form is already open

---
+++ CarRentApp1/CarRentApp1/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentApp1
+{
+    public static class Utils
+    {
+        public static bool FormIsOpen(string name)
+        {
+            //check if window is already open
+            var openForms = Application.OpenForms.Cast<Form>();
+            var isOpen = openForms.Any(x => x.Name == name);
+            return isOpen;
+        }
+    }
+}
