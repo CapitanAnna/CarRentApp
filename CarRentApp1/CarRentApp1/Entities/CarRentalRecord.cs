012d52f1e48f62e1246cb7b5c0db7fe2dda2ca01
CapitanAnna <capitanstil@gmail.com>
2022-02-09 11:26:03 +0200

    Add Entities

---
+++ CarRentApp1/CarRentApp1/Entities/CarRentalRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarRentApp1.Entities
+{
+    public class CarRentalRecord
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime? DateRented { get; set; }
+        public DateTime? DateReturned { get; set; }
+        public decimal Cost { get; set; }
+        public int? TypesOfCarId { get; set; }
+        public virtual TypesOfCar TypesOfCars { get; set; }
+        
+    }
+}
---
+++ CarRentApp1/CarRentApp1/Entities/TypesOfCar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentApp1.Entities
+{
+    public class TypesOfCar
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }//Brand
+        public string VIN { get; set; }
+        public string LicensePlateNumber { get; set; }
+        public int? Year { get; set; }
+
+        private ICollection<CarRentalRecord> CarRentalRecords;
+
+        
+    }
+}
